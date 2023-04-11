ac759c41b5cd648b88add84b6b811a60044c370b
Sergio Mendes <srgmendesl@gmail.com>
2023-04-10 22:57:30 -0300

    inicio da pagina principal

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg" style="background-color:#20B2AA">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Ophtalmus</a>
+              <div class="collapse navbar-collapse nav-fill justify-content-center" id="navbarSupportedContent">
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2 " type="search" placeholder="Pesquisar" aria-label="Search">
+                  <button class="btn btn-light" type="submit"><span class="material-symbols-outlined">search</span></i></button>
+                </form>
+              </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Nome do Usuário
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Trocar de Usuário</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Log Out</a></li>
+                    </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
