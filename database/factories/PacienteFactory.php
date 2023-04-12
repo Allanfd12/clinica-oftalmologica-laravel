fc59f518b5d530e6dccede6530cbc11925589a81
Allan França <allanfd12@gmai.com>
2023-04-11 21:36:53 -0300

    add paciente -> migration model factory seeder

---
+++ database/factories/PacienteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Paciente>
+ */
+class PacienteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        
+        $paciente_pessoa_id = $this->faker->unique()->numberBetween(1, 100);
+        return [
+            'pessoa_id' => $paciente_pessoa_id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        \App\Models\Endereco::factory(100)->create();
-        \App\Models\Pessoa::factory(100)->create();
-        
     }
 }
