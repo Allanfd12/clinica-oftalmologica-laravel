df1b920c9bff43a8f5028be203cf07d828c09d2d
Fernanda Barbosa <nandaferrazbmf@gmail.com>
2023-04-15 12:43:00 -0300

    Adicionando arquivos pro crud paciente

--- resources/views/pacientes/list.blade.php
+++ resources/views/pacientes/list.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', 'Ophtamuls - Pacientes')
 
-@section('content')
-    <h1>pacientes</h1>
+@section('content')       
+    <h1 style="text-align: left; margin-top: 2%; margin-left: 5%; color: #20B2AA">Pacientes</h1>
+    <hr style="margin: 5%; margin-top: 2%"class="linha-home">
 @endsection
