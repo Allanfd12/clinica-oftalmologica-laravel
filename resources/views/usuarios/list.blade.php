a791c0073eca787278883c3dbfd9e439b262d037
Sergio Mendes <srgmendesl@gmail.com>
2023-04-15 00:35:17 -0300

    titulo de list usuarios

--- resources/views/usuarios/list.blade.php
+++ resources/views/usuarios/list.blade.php
@@ -3,5 +3,7 @@
 @section('title', 'Ophtamuls - Usuários')
 
 @section('content')
-
+    
+    <h1 style="text-align: left; margin-top: 2%; margin-left: 5%; color: #20B2AA">Usuários</h1>
+    <hr style="margin: 5%; margin-top: 2%"class="linha-home">
 @endsection
