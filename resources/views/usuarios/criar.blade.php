ae4005242f7080ef171ad9dd320671ca4f0d834d
Sergio Mendes <srgmendesl@gmail.com>
2023-04-18 00:18:50 -0300

    começando a tabela de criar usuário

---
+++ resources/views/usuarios/criar.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.public')
+
+@section('title', 'Ophtamuls - Usuários')
+
+@section('content')
+    <h2 style="text-align: center; margin-top: 2%; color: #20B2AA">Criar Usuário</h2>
+    <hr style="margin-top: 1%; margin-bottom: 1%;" class="linha-home">
+
+    <div class="card" style="width: 75%; margin: auto">
+        <h5 class="card-header d-flex justify-content-between align-items-center" style="background-color:#20B2AA; color: white;">
+            Preencha os dados do novo usuário
+        </h5>
+        <div class="card-body">
+            
+        </div>
+    </div>
+@endsection
