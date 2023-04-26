604964c65ea9ac04765908e55c0b983da4678919
Sergio Mendes <srgmendesl@gmail.com>
2023-04-25 22:06:46 -0300

    forms cadastrar front feito

--- resources/views/usuarios/criar.blade.php
+++ resources/views/usuarios/criar.blade.php
@@ -6,7 +6,7 @@
     <h2 style="text-align: center; margin-top: 2%; color: #20B2AA">Criar Usuário</h2>
     <hr style="margin-top: 1%; margin-bottom: 1%;" class="linha-home">
 
-    <div class="card" style="width: 85%; margin: auto">
+    <div class="card" style="width: 90%; margin: auto">
         <h4 class="card-header d-flex justify-content-between align-items-center" style="background-color:#20B2AA; color: white;">
             Preencha os dados do novo usuário
         </h4>
@@ -38,6 +38,38 @@
                     </div>
                 </div>
                 <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <label for="rua" class="form-label">Rua</label>
+                        <input type="text" class="form-control" name="rua" >
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="numero" class="form-label">Número</label>
+                        <input type="text" class="form-control" name="numero" >
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="cep" class="form-label">CEP</label>
+                        <input type="text" class="form-control" name="cep" >
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <label for="bairro" class="form-label">Bairro</label>
+                        <input type="text" class="form-control" name="bairro" >
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="cidade" class="form-label">Cidade</label>
+                        <input type="text" class="form-control" name="cidade" >
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="estado" class="form-label">Estado</label>
+                        <input type="text" class="form-control" name="estado" >
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 mb-4">
+                        <label for="complemento" class="form-label">Complemento</label>
+                        <input type="text" class="form-control" name="complemento">
+                    </div>
                     <div class="col-md-4 mb-4">
                         <label for="password" class="form-label">Senha</label>
                         <input type="tel" class="form-control" name="password" >
