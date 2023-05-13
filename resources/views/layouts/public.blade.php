48645537478a01ac65c27e9bfd26a520f257759b
Allan França <allanfd12@gmai.com>
2023-05-12 21:43:02 -0300

    add jquery mask

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -114,6 +114,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
+    <script src="{{ asset('js/jquery.mask.min.js') }}"></script>
     <script>
         $('.sidebar ul li').on('click', function() {
             $('.sidebar ul li.active').removeClass('active');
