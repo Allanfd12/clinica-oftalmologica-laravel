3237df910f9d08704e1e7cfcb81ede675e2134a6
Allan França <allanfd12@gmai.com>
2023-05-16 19:18:27 -0300

    cores login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,13 +28,13 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 text-white">{{ __('Lembrar de Mim') }}</span>
+                <span class="ml-2 text-sm text-gray-600 text-black">{{ __('Lembrar de mim') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm" style="color: white;" href="{{ route('password.request') }}">
+                <a class="underline text-sm" style="color: black;" href="{{ route('password.request') }}">
                     {{ __('Esqueceu sua Senha?') }}
                 </a>
             @endif
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,15 +15,16 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 white">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 " style="
+        background-color: #20B2AA;">
             <div>
                 <a href="/">
                     <span style="font-size: 40px;
-                        color: #20B2AA;"> Ophtamuls</span>
+                        color: #fff;"> Ophtamuls</span>
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-color: #20B2AA">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-color: #fff">
                 {{ $slot }}
             </div>
         </div>
